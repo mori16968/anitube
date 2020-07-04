bc227e9034fb859f7bbeca43262300bf4978e0c0
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-07-04 16:59:39 +0900

    シードデータにリレーションシップを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-10.times do |n|
+# ユーザー
+50.times do |n|
   name = Faker::Name.name
   email = "test#{n + 1}@test.com"
   password = "password"
@@ -9,8 +10,17 @@
                )
 end
 
+# ユーザープロフィール画像
 users = User.order(:id).take(10)
 users.each_with_index do |user, i|
   user.avatar.attach(io: File.open(Rails.root.join("db/fixtures/avatar-#{i + 1}.jpg")), filename: "avatar-#{i + 1}.jpg")
   user.save
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..30]
+followers = users[3..20]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
