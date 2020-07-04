3b206da63b9fa31957b18b286b3a65d8f607a669
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-07-04 22:33:41 +0900

    シードデータにPostを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 # ユーザー
 50.times do |n|
   name = Faker::Name.name
@@ -13,7 +15,7 @@ end
 # ユーザープロフィール画像
 users = User.order(:id).take(10)
 users.each_with_index do |user, i|
-  user.avatar.attach(io: File.open(Rails.root.join("db/fixtures/avatar-#{i + 1}.jpg")), filename: "avatar-#{i + 1}.jpg")
+  user.avatar.attach(io: File.open(Rails.root.join("db/fixtures/avatar/avatar-#{i + 1}.jpg")), filename: "avatar-#{i + 1}.jpg")
   user.save
 end
 
@@ -24,3 +26,18 @@ following = users[2..30]
 followers = users[3..20]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+# Post
+users = User.order(:id).take(5)
+i = 0
+(1).upto(5) do |n|
+  CSV.foreach("db/csv/post_user_#{n}.csv", headers: true) do |row|
+    users[i].posts.create!(
+      youtube_url: row['youtube_url'],
+      title: row['title'],
+      body: row['body'],
+      created_at: rand(Time.zone.yesterday.beginning_of_day..Time.zone.yesterday.end_of_day)
+    )
+  end
+  i += 1
+end
