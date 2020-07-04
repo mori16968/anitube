20ce37d5aba6fc9db4f69891019d26cb62b6eb56
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-07-04 15:00:58 +0900

    シードデータに名前とプロフィール画像追加

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'rails-i18n', '~> 5.1'
 gem 'devise'
 gem 'devise-i18n'
 gem 'devise-bootstrap-views', '~> 1.0'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
-User.create(name: '石川界人', email: 'ishikawa@test.com', password: 'password')
-User.create(name: '岡本信彦', email: 'okamoto@test.com', password: 'password')
-User.create(name: '梶裕貴', email: 'kaji@test.com', password: 'password')
-User.create(name: '花江夏樹', email: 'hanae@test.com', password: 'password')
-User.create(name: '藤原啓治', email: 'hujiwara@test.com', password: 'password')
+30.times do |n|
+  name = Faker::Name.name
+  email = "test#{n + 1}@test.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+users = User.order(:id).take(10)
+users.each_with_index do |user, i|
+  user.avatar.attach(io: File.open(Rails.root.join("db/fixtures/avatar-#{i + 1}.jpg")), filename: "avatar-#{i + 1}.jpg")
+  user.save
+end
