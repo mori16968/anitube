c83418e161a43ed2222e96f7f0af695ff9d84053
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-07-11 22:33:05 +0900

    Postのシステムテスト追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :system do
-  
+  let!(:user) { create(:user) }
+
+  before do
+    log_in(user)
+  end
+
+  it "新規投稿を作成、編集、削除できること" do
+    click_link '投稿する'
+
+    expect(current_path).to eq new_post_path
+    expect(page).to have_content '新規投稿'
+
+    # 新規投稿
+    fill_in '動画URL', with: 'https://youtu.be/Zzv42rrtUbM'
+    fill_in 'タイトル', with: 'リゼロ二期'
+    fill_in '本文', with: 'とても楽しみ'
+    click_button '投稿'
+
+    post = Post.first
+    aggregate_failures do
+      expect(post.youtube_url).to eq 'https://youtu.be/Zzv42rrtUbM'
+      expect(post.title).to eq 'リゼロ二期'
+      expect(post.body).to eq 'とても楽しみ'
+      expect(current_path).to eq posts_path
+    end
+
+    # 編集
+    click_link 'リゼロ二期'
+    expect(current_path).to eq post_path(post.id)
+    click_link '編集'
+    expect(current_path).to eq edit_post_path(post.id)
+    expect(page).to have_content '投稿編集'
+
+    fill_in '本文', with: 'あまり楽しみでない'
+    click_button '更新'
+
+    expect(current_path).to eq post_path(post.id)
+    expect(page).to have_content 'あまり楽しみでない'
+    expect(page).to_not have_content 'とても楽しみ'
+
+    # 削除
+    click_link '削除'
+    aggregate_failures do
+    expect(current_path).to eq posts_path
+    expect(page).to_not have_content 'リゼロ二期'
+    expect(Post.where(id: post.id)).to be_empty
+    end
+  end
 end
