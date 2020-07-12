25ac2ab5a71d7a85c24b5eb81ea7585bbeeaafa4
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-07-12 18:38:10 +0900

    投稿完了時にflashメッセージ追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
+      flash[:success] =  "投稿が完了しました"
       redirect_to posts_path
     else
       render 'posts/new'
