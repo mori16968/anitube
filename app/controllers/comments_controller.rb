7bd89391996d04b9dc29e91d21ec8c1f69f6dcaa
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-07-12 17:01:35 +0900

    コメントコントローラーのpost_paramsに値を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :post_id, :user_id)
   end
 end
