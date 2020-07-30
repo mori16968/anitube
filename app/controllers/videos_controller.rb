351bf262b69896879774ca9556a545a16555b5b4
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-07-31 06:05:04 +0900

    change: APIキーを.envで記述

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-  GOOGLE_API_KEY = 'AIzaSyB96X73K0OUQh8dgtImU1iKQ91e2dEXUg4'
+  GOOGLE_API_KEY = ENV['MY_GOOGLE_API_KEY']
 
   def find_videos(keyword, after: 1.months.ago, before: Time.now)
     service = Google::Apis::YoutubeV3::YouTubeService.new
