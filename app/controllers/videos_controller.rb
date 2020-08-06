c0899c49052761f3e433498292d78705b6976d60
Takahiko Nakahama <altair.vega16968@gmail.com>
2020-08-07 00:56:02 +0900

    update: Videoモデル作成、APIからのデータをDBに保存してshowページを追加

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,27 +1,10 @@
 class VideosController < ApplicationController
-  GOOGLE_API_KEY = ENV['MY_GOOGLE_API_KEY']
-
-  def find_videos(keyword, after: 1.months.ago, before: Time.now)
-    service = Google::Apis::YoutubeV3::YouTubeService.new
-    service.key = GOOGLE_API_KEY
-
-    next_page_token = nil
-    opt = {
-      q: keyword,
-      type: 'video',
-      max_results: 8,
-      order: :relevance,
-      page_token: next_page_token,
-      published_after: 3.month.ago.iso8601,
-      video_category_id: 1
-    }
-    service.list_searches(:snippet, opt)
-  end
 
   def index
-    @youtube_data = find_videos('アニメ 公式 PV')
+    @videos = Video.all
   end
 
   def show
+    @video = Video.find(params[:id])
   end
 end
